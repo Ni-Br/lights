693bba309e5c14a805332af930c284705612ed06
Olivia Brode-Roger <nibr@mit.edu>
2015-07-29 06:24:42 -0400

    Client now takes IP and port arguments

--- device/device.py
+++ device/device.py
@@ -4,9 +4,11 @@ import fileinput
 import socket
 import time
 
-IP = "localhost"
-PORT = 9999
+IP = sys.argv[1]
+PORT = int(sys.argv[2])
 BUFFER_SIZE = 20
+count = 0
+start = 0
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((IP, PORT))
@@ -15,17 +17,19 @@ s.listen(1)
 conn, addr = s.accept()
 sfile = conn.makefile("rwb")
 print("Connection from:", addr)
-count = 0
 start = time.time()
-while True:
-    data = sfile.readline().strip()
-    if not data:
-        break
 
-    print("Received", data)
-    conn.sendall((str(time.time()) + "\n").encode())
-    count +=1
+try:
+    while True:
+        data = sfile.readline().strip()
+        if not data:
+            break
+
+        print("Received", data)
+        conn.sendall((str(time.time()) + "\n").encode())
+        count +=1
+except:
+    conn.close()
 
 end = time.time()
 print(count, "frames in", end-start, "seconds. FPS of", count/(end-start))
-conn.close()
